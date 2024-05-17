c0a4e51e438a022569e54501034b4249424616dd
sufyan360 <117491069+sufyan360@users.noreply.github.com>
2024-05-17 11:30:44 -0500

    flask IMPLEMENTED

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import dotenv
 import os
-from flask import Flask, render_template, flash
-from googleplaces import GooglePlaces
-from search import LocationInputForm
+from flask import Flask, render_template
 
 dotenv.load_dotenv()
 gtoken = str(os.getenv("GTOKEN"))
@@ -10,27 +8,18 @@ flasktoken = str(os.getenv("FLASKTOKEN"))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = flasktoken
-gglObj = GooglePlaces(gtoken)
 
-
-@app.route('/', methods=["GET", "POST"])
-def home():  # put application's code here
-    form = LocationInputForm()
-    if form.validate_on_submit():
-        query = gglObj.nearby_search(location=form.town.data, keyword=form.topic.data, radius=5000)
-        UserLocation = form.town.data
-        for place in query.places:
-            place.get_details()
-        flash(f"Searching for {form.topic.data} ", "success")
-        return render_template("ResultingMaps.html", places=query.places, userlocation=UserLocation)
-    else:
-        flash("Query Unsuccessful. Please check Town and Topic", "danger")
-    return render_template("home.html", name="Home", form=form)
+@app.route('/')
+def home():
+    return render_template('home.html', name = "Home")
 
 @app.route('/aboutMe')
-def about():  # put application's code here
-    return render_template('aboutMe.html', name="About Me")
+def about():
+    return render_template('aboutMe.html', name = "About")
 
+@app.route('/resume')
+def resume():
+    return render_template('resumePage.html', name = "Resume")
 
 if __name__ == '__main__':
     app.run()
